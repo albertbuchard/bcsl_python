51d91d5de9005a941688a390be7d1eb6229bafce
albertbuchard <albert.buchard@gmail.com>
2024-10-18 03:29:09 +0200

    version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="bcsl_python",
-    version="0.7.0",
+    version="0.8.0",
     author="Guo, Xianjie",
     author_email="albert.buchard@gmail.com",
     description="Bootstrap-based Causal Structure Learning",
